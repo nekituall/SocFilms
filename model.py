bbdaeff4da25d73ec76f4b41710bd6d948318487
nekituall <nekituall@gmail.com>
2023-03-17 00:42:15 +0300

    - search film superrefactor for adding to DB immediately  - search user refactor  - search friend refactor

--- model.py
+++ model.py
@@ -76,15 +76,18 @@ def search_user(name):
     """поиск пользователя по никнейму"""
     conn = create_conn(config)
     cur = conn.cursor()
-    cur.execute("SELECT name, surname, nickname, country FROM users WHERE nickname=%s;", (name,))
+    cur.execute("SELECT idusers, name, surname, nickname, country FROM users WHERE nickname=%s;", (name,))
     user_data = cur.fetchone()
-    if len(user_data) != 0:
-        close_db(conn)
-        print(user_data)
-        return user_data
-    else:
-        print("No such user")
-        return "No such user"
+    try:
+        if len(user_data) != 0 and user_data is not None:
+            close_db(conn)
+            print(user_data)
+            return user_data
+        else:
+            print("No such user")
+            return "No such user"
+    except:
+        return None
 
 
 def login_user(name, passw):
@@ -106,7 +109,12 @@ def show_friends(user_id, status):
     conn = create_conn(config)
     cur = conn.cursor()
     cur.execute("""SELECT us.name, us.surname, us.country, fr.status FROM users as us JOIN friends as fr 
-    ON us.idusers=fr.friend_user WHERE fr.main_user=%s AND fr.status=%s;""", (user_id, status))
+    ON us.idusers=fr.main_user WHERE fr.friend_user=%s AND fr.status=%s UNION SELECT us.name, us.surname, us.country, fr.status FROM users as us JOIN friends as fr 
+    ON us.idusers=fr.friend_user WHERE fr.main_user=%s  AND fr.status=%s;""", (user_id, status, user_id, status))
+    # cur.execute("""SELECT us.name, us.surname, us.country, fr.status FROM users as us JOIN friends as fr
+    # ON us.idusers=fr.main_user WHERE fr.friend_user=%s AND fr.status=%s;""", (user_id, status))
+    # cur.execute("""SELECT us.name, us.surname, us.country, fr.status FROM users as us JOIN friends as fr
+    #     ON us.idusers=fr.friend_user WHERE fr.main_user=%s AND fr.status=%s;""", (user_id, status))
     friends = cur.fetchall()
     if len(friends) != 0:
         close_db(conn)
@@ -117,12 +125,14 @@ def show_friends(user_id, status):
         print("No friends yet..")
         return None
 
+# def ask_friend(values: tuple):
 def ask_friend(values):
     """запросить дружбу
     values кортеж = (отправитель запроса, получатель запроса)
     """
     conn = create_conn(config)
     cur = conn.cursor()
+    # тут можно сделать проверку есть ли уже пользователь в друзьях перед запросом
     query = ("INSERT INTO friends (main_user, friend_user) VALUES (%s,%s);")
     cur.execute(query, values)
     conn.commit()
@@ -164,31 +174,63 @@ def delete_friend(values):
 # ROW_COUNT() для тестов
 
 
-def search_film(name) -> list:
-    """ищем фильм в базе данных, если нету то по АПИ"""
+def search_film(name):
+    """ищем фильм в базе данных, сравниваем с АПИ, если в базе нет, то добавляем его"""
+
     conn = create_conn(config)
     cur = conn.cursor()
-    query = """SELECT filmname, year, genre, countryname FROM films f JOIN genrefilms gs ON 
-    f.idfilms=gs.id_films JOIN genres g ON gs.id_genre=g.idgenres JOIN countryfilms cs ON f.idfilms=cs.id_film 
-    JOIN countries c ON cs.id_country=c.idcountries WHERE filmname=%s GROUP BY f.idfilms;"""
+    query = """SELECT filmname, year, (SELECT group_concat(genre SEPARATOR ',') FROM genres g JOIN genrefilms gs ON g.idgenres=gs.id_genre WHERE gs.id_films = f.idfilms) AS genre,
+    (SELECT group_concat(countryname SEPARATOR ',') FROM countries c JOIN countryfilms cs ON c.idcountries=cs.id_country WHERE cs.id_film = f.idfilms) AS countryname FROM films f JOIN genrefilms gs ON
+        f.idfilms=gs.id_films JOIN genres g ON gs.id_genre=g.idgenres JOIN countryfilms cs ON f.idfilms=cs.id_film
+        JOIN countries c ON cs.id_country=c.idcountries WHERE filmname=%s GROUP BY f.idfilms;"""
     cur.execute(query, (name,))
-    # чтобы вывести список похожий на апи с одним жанром и страной
-    # "SELECT films.filmname, films.year,  FROM films WHERE filmname=%s;"
-    res = cur.fetchall()
-    print(res)
-    if len(res) == 0:
-        print("api")
-        #запрос к АПИ
-        data = search_api(name)
+    db_data = cur.fetchall()
+    api_data = search_api(name)
+
+    db_final = []
+    for db in db_data:
+        db = list(db)
+        db[2] = db[2].split(",")
+        db[3] = db[3].split(",")
+        db_final.append(db)
+    # print(db_final)
+
+    api_final = [list(api) for api in api_data]
+    # print(api_final)
+    new_data = [x for x in api_final if x not in db_final]  # это данные, которых нету в базе
+    # print(new_data)
+
+    for data in new_data:
         print(data)
-        return data
-    else:
-        print("я в бд")
-        close_db(conn)
-        return res
+        add_film(data)
 
 
-def add_film(value: tuple):
+    #СТАРАЯ ВЕРСИЯ поиска по апи
+
+    # conn = create_conn(config)
+    # cur = conn.cursor()
+    # # query = """SELECT filmname, year, genre, countryname FROM films f JOIN genrefilms gs ON
+    # # f.idfilms=gs.id_films JOIN genres g ON gs.id_genre=g.idgenres JOIN countryfilms cs ON f.idfilms=cs.id_film
+    # # JOIN countries c ON cs.id_country=c.idcountries WHERE filmname=%s GROUP BY f.idfilms;"""
+    # # cur.execute(query, (name,))
+    # # # чтобы вывести список похожий на апи с одним жанром и страной
+    # cur.execute("SELECT filmname, year FROM films WHERE filmname=%s;", (name,))
+    # res = cur.fetchall()
+    # print(res)
+    # # всегда через АПИ и наполнять базу
+    # if len(res) == 0:
+    #     print("api")
+    #     #запрос к АПИ
+    #     data = search_api(name)
+    #     print(data)
+    #     return data
+    # else:
+    #     print("я в бд")
+    #     close_db(conn)
+    #     return res
+
+
+def add_film(value: list):
     """Добавить  фильм
     при добавлении фильма проверяются таблицы жанров и стран, чтобы не было дубликатов
     """
@@ -278,14 +320,14 @@ if __name__ == "__main__":
     # deploy_db(config_deploy)      # работает
     # create_user(d)                # работает
     # login_user("niknik","passw")    # работает
-    search_user("nekituall")       # работает
+    # search_user("nekituall")       # работает
     # ask_friend((3, 1))          # работает
-    # show_friends(3, "confirmed")    # работает
+    # show_friends(2, "confirmed")    # работает
     # confirm_friend((3,2))       # работает
     # reject_friend((3, 2))       # работает
     # delete_friend((3, 4))       # работает
-    # search_film(("Титаник",))      # работает
-    # add_film(film3)               # работает
+    search_film("Титаник")      # работает
+    # add_film(film1)               # работает
     # add_favourite((3, 3, "2023-04-11", 5, "что надо"))       #работает , если дата-строчка
     # show_favourites((1,))
     # delete_favourite((12, 1))       # работает
